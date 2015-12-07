796485c41e82c3a831cdd8f24feca96e4939ca10
Guillermo Ramos <guillermoramos330179@gmail.com>
2015-12-07 13:45:03 -0500

    add framework to create a haiku

---
+++ Poetry.py
@@ -0,0 +1,23 @@
+import nltk.data
+from nltk.corpus import wordnet as wn
+from nltk.corpus import cmudict
+import pickle
+from Queue.py import Queue
+
+class Poetry_finder():
+    def __init__(self):
+        self.inputfile = None
+        self.queue = Queue()
+
+    def makeHaiku(self):
+        """This function will find a haiku based on an input file"""
+
+        return 0
+    def sentence_finder(self):
+        """This function will divide the input file into sentences and organize them"""
+
+       return 0
+    def find_component(self, syl):
+        """This function will find a sentence that contains the number of syllables needed puts them into queues divided up into haikus"""
+        return 0
+
