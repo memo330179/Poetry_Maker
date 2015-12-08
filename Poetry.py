88be396f57f248ab00bfb0127cae533775b0796e
Guillermo Ramos <guillermoramos330179@gmail.com>
2015-12-07 22:04:51 -0500

    hopefully work

--- Poetry.py
+++ Poetry.py
@@ -1,23 +1,100 @@
+import nltk
 import nltk.data
 from nltk.corpus import wordnet as wn
 from nltk.corpus import cmudict
 import pickle
-from Queue.py import Queue
+from queue import Queue
+import re
+import curses
+from curses.ascii import isdigit
+import string
 
 class Poetry_finder():
-    def __init__(self):
-        self.inputfile = None
+    def __init__(self, inputfile = "test.txt"):
+        self.inputfile = inputfile
         self.queue = Queue()
+        self.lines = []
+        self.line_no_punct = []
+        self.words = []
 
-    def makeHaiku(self):
+    def prepare(self):
         """This function will find a haiku based on an input file"""
+        self.sentence_finder()
+        self.strip_punctuation()
+        self.split_words()
 
-        return 0
+    def findHaiku(self):
+        state = 0
+        for sentence in self.words:
+            syls = self.syllable_count(sentence)
+            if (state ==  0) and  (syls == 5):
+                self.queue.enqueue(sentence)
+                state = 1
+            elif (state == 1) and (syls == 7):
+                self.queue.enqueue(sentence)
+                state = 2
+            elif (state == 2) and (syls == 5):
+                self.queue.enqueue(sentence)
+                state = 3
+            if state == 3:
+                self.queue.enqueue("------------")
+                state = 0
+        self.printHaiku()
+    def printHaiku(self):
+        for i in range(self.queue.size()):
+            print (self.queue.dequeue())
     def sentence_finder(self):
         """This function will divide the input file into sentences and organize them"""
+        tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+        fp = open(self.inputfile)
+        data = fp.read()
 
-       return 0
+        self.lines= tokenizer.tokenize(data)
+
     def find_component(self, syl):
         """This function will find a sentence that contains the number of syllables needed puts them into queues divided up into haikus"""
         return 0
+    def strip_punctuation(self):
+        """strips the punctuation of a sentence so that python does not get confused"""
+        sentence_list = []
+        for sentences in self.lines:
+            lower_case = str.lower(sentences)
+            #sentence_list.append([lower_case.translate(str.maketrans("",""), string.punctuation)])
+            sentence_list.append([re.sub("[\.\t\,\:;\(\)\.\!\?]", "", lower_case, 0, 0)])
 
+        self.line_no_punct = sentence_list
+    def syllable_count(self, sentence):
+        """counts syllables in a sentence"""
+
+        d = cmudict.dict()
+        def nysl(word):
+
+           return[len(list(y for y in x if isdigit(y[-1]))) for x in d[word.lower()]]
+        count = 0
+        for words in sentence:
+            count += nysl(words)[0]
+        return count
+    def split_words(self):
+        for sentences in self.line_no_punct:
+            self.words.append(sentences[0].split())
+def main():
+    sentences = Poetry_finder("hhgttg.txt")
+    #sentences.sentence_finder()
+    #print(sentences.lines)
+    #striped = sentences.strip_punctuation()
+    #sentences.split_words()
+    #d = sentences.words
+    #k = sentences.syllable_count(d[0])
+    sentences.prepare()
+    sentences.findHaiku()
+
+    #print (k)
+    #print(striped)
+    #d = cmudict.dict()
+    #sentence = "denote"
+    #def nsyl(word):
+    #    for x in d[word.lower()]
+    #        return len(list(y for y in x if isdigit(y[-1])))
+    #print(nsyl(sentence))
+
+main()
--- queue.py
+++ queue.py
@@ -2,21 +2,21 @@
 # Dave Reed 02/24/2006
 #
 # from Data Structures and Algorithms Using Python and C++
-# downloaded from publisher's website: 
-# https://www.fbeedle.com/content/data-structures-and-algorithms-using-python-and-c 
+# downloaded from publisher's website:
+# https://www.fbeedle.com/content/data-structures-and-algorithms-using-python-and-c
 # on July 23, 2014
-# 
+#
 
 #----------------------------------------------------------------------
 
 class Queue:
 
     #----------------------------------------------------------------------
-    
+
     def __init__(self):
 
         '''create an empty FIFO queue'''
-        
+
         self.q = []
 
     #----------------------------------------------------------------------
@@ -26,9 +26,9 @@ class Queue:
         '''return number of items in the queue
 
         pre: none
-        
+
         post: returns number of items in the queue'''
-        
+
         return len(self.q)
 
     #----------------------------------------------------------------------
@@ -40,7 +40,7 @@ class Queue:
         pre: none
 
         post: x is added to the queue'''
-        
+
         self.q.append(x)
 
     #----------------------------------------------------------------------
@@ -64,7 +64,7 @@ class Queue:
         pre: queue is not empty; IndexError is raised if empty
 
         post: removes and returns first item in the queue'''
-        
+
         return self.q.pop(0)
 
 #----------------------------------------------------------------------
