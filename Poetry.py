73255db43202ce53d4580824fb89059397b1d768
Guillermo Ramos <guillermoramos330179@gmail.com>
2015-12-08 23:29:53 -0500

    works

--- Poetry.py
+++ Poetry.py
@@ -8,6 +8,8 @@ import re
 import curses
 from curses.ascii import isdigit
 import string
+import urllib
+from bs4 import BeautifulSoup
 
 class Poetry_finder():
     def __init__(self, inputfile = "test.txt"):
@@ -16,28 +18,35 @@ class Poetry_finder():
         self.lines = []
         self.line_no_punct = []
         self.words = []
+        self.tmpQueue = Queue()
 
     def prepare(self):
         """This function will find a haiku based on an input file"""
         self.sentence_finder()
-        self.strip_punctuation()
-        self.split_words()
 
     def findHaiku(self):
         state = 0
-        for sentence in self.words:
-            syls = self.syllable_count(sentence)
+        for sentence in self.lines:
+            syls = self.syllable_count_fail(sentence)
             if (state ==  0) and  (syls == 5):
                 self.queue.enqueue(sentence)
+                self.tmpQueue.enqueue(sentence)
+
                 state = 1
             elif (state == 1) and (syls == 7):
-                self.queue.enqueue(sentence)
-                state = 2
+                 self.tmpQueue.enqueue(sentence)
+                 self.queue.enqueue(sentence)
+                 state = 2
             elif (state == 2) and (syls == 5):
                 self.queue.enqueue(sentence)
+                self.tmpQueue.enqueue(sentence)
                 state = 3
             if state == 3:
                 self.queue.enqueue("------------")
+                for i in range(self.tmpQueue.size()):
+                    line = self.tmpQueue.dequeue()
+                    print(line)
+                print("--------")
                 state = 0
         self.printHaiku()
     def printHaiku(self):
@@ -68,17 +77,47 @@ class Poetry_finder():
 
         d = cmudict.dict()
         def nysl(word):
-
-           return[len(list(y for y in x if isdigit(y[-1]))) for x in d[word.lower()]]
+            return[len(list(y for y in x if isdigit(y[-1]))) for x in d[word.lower()]]
         count = 0
         for words in sentence:
-            count += nysl(words)[0]
+            try:
+                count += nysl(words)[0]
+            except KeyError:
+                count += self.syllable_count_fail(words)
         return count
+
+    def syllable_count_fail(self, word):
+        url = 'http://www.wordcalc.com/index.php'
+        pala = {'text': word }
+        post_data = urllib.parse.urlencode(pala)
+        post_data = '%s&optionSyllableCount&optionWordCount' % post_data
+        binary_data = post_data.encode('ascii')
+        cnxn = urllib.request.urlopen(url, binary_data)
+        response = cnxn.read()
+        cnxn.close()
+
+        soup = BeautifulSoup(response, "html.parser")
+        h3_matches = [h3 for h3 in soup.findAll('h3') if h3.text == 'Statistics']
+        if len(h3_matches) != 1:
+              raise Exception('Wrong number of <h3>Statistics</h3>')
+        h3_match = h3_matches[0]
+        table = h3_match.findNextSibling('table')
+
+        td_matches = [td for td in table.findAll('td')
+                        if td.text == 'Syllable Count']
+        if len(td_matches) != 1:
+            raise Exception('Wrong number of <td>Syllable Count</td>')
+        td_match = td_matches[0]
+
+        td_value = td_match.findNextSibling('td')
+        syllable_count = int(td_value.text)
+        return syllable_count
     def split_words(self):
         for sentences in self.line_no_punct:
             self.words.append(sentences[0].split())
+
 def main():
-    sentences = Poetry_finder("hhgttg.txt")
+    sentences = Poetry_finder("Oedipus_Rex.txt")
     #sentences.sentence_finder()
     #print(sentences.lines)
     #striped = sentences.strip_punctuation()
@@ -87,7 +126,8 @@ def main():
     #k = sentences.syllable_count(d[0])
     sentences.prepare()
     sentences.findHaiku()
-
+    #count = sentences.syllable_count_fail("denote")
+    #print(count)
     #print (k)
     #print(striped)
     #d = cmudict.dict()
