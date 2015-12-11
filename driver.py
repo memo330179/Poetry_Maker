805750822768ea6c24c9788e884615213739f588
Guillermo Ramos <guillermoramos330179@gmail.com>
2015-12-10 23:24:24 -0500

    fixed driver

--- driver.py
+++ driver.py
@@ -1,7 +1,7 @@
 
-import nltk 
-import nltk.data 
-from nltk.corpus import wordnet as wn  
+import nltk
+import nltk.data
+from nltk.corpus import wordnet as wn
 from nltk.corpus import cmudict
 from queue import Queue
 import re
@@ -11,27 +11,26 @@ import string
 import urllib
 from bs4 import BeautifulSoup
 import os
-import Poetry.py
 from Poetry import Poetry_finder
 
 
 
 
 def main():
-    text = raw_input("What is the file you want to make poems from? Include .txt "  )
+    text = input("What is the file you want to make poems from? Include .txt "  )
     sentences = Poetry_finder(text)
     sentences.prepare()
-    
-    choice = raw_input("Type 1 to make an Acrostic, 2 to make a haiku: " )
-    
-    
+
+    choice = int(input("Type 1 to make an Acrostic, 2 to make a haiku: " ))
+
+
     if choice == 1:
-        acrostic = raw_input("what word would you like to make the acrostic? ")
+        acrostic = input("what word would you like to make the acrostic? ")
         sentences.findAcrostic(acrostic)
-    
+
     else:
-    
+
         sentences.findHaiku()
-    
-    
-main() 
+
+
+main()
